ff44f5ccf1ead8142892bfc43e6ab6b19742c37b
dannysofftie <dankim761@gmail.com>
2021-05-30 18:25:49 +0300

    Update transaction with confirmation blocks

--- api/src/resolvers/mutations/invoice.ts
+++ api/src/resolvers/mutations/invoice.ts
@@ -2,9 +2,27 @@ import { IResolvers } from 'mercurius';
 
 const invoiceMutations: IResolvers['Mutation'] = {
   async updateInvoiceTransaction(root, args, ctx, info) {
-    console.log({
-      transaction: args.input,
-    });
+    // update transaction hash
+    if (args.input?.transactionHash) {
+      const exists = await ctx.app.models.Transaction.findOne({ transactionHash: args.input.transactionHash });
+      if (!exists) {
+        // create this transaction
+        await ctx.app.models.Transaction.create({
+          invoice: args.where.id,
+          transactionHash: args.input.transactionHash,
+        });
+      }
+    }
+
+    // if blocks, add to existing transaction document
+    if (args.input?.block) {
+      await ctx.app.models.Transaction.updateOne(
+        // intentional line break
+        { transactionHash: args.input.block.transactionHash },
+        { $push: { blocks: args.input.block } }
+      );
+    }
+
     ctx.pubsub.publish({
       topic: 'invoiceUpdated',
       payload: {
--- api/src/resolvers/queries/invoices.ts
+++ api/src/resolvers/queries/invoices.ts
@@ -3,9 +3,18 @@ import { IResolvers } from 'mercurius';
 const invoiceQueries: IResolvers['Query'] = {
   async invoices(root, args, ctx, info) {
     return await ctx.app.models.Invoice.aggregate([
-      // intentional line break
       { $set: { id: '$_id' } },
       { $unset: ['_id'] },
+      {
+        $lookup: {
+          from: 'transactions',
+          let: { id: '$id' },
+          pipeline: [{ $match: { $expr: { $eq: ['$$id', '$invoice'] } } }],
+          as: 'transactions',
+        },
+      },
+      { $set: { transaction: { $arrayElemAt: ['$transactions', 0] } } },
+      { $unset: ['transactions'] },
     ]);
   },
 };
